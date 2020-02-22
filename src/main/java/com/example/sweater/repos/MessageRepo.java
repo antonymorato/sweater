ee19fd20a06a195e42579ad1882ff85d4d00f346
antony <antonymorato2@gmail.com>
2020-02-22 17:57:19 +0200

    first commit, added messageRepo

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,14 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository<Message,Long> {
+
+
+
+
+
+
+
+}
